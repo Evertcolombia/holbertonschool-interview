1177347450ea8773389fb8892a0fa8248da7c4a8
evertcolombia <evertescalante@gmail.com>
2021-04-22 17:45:06 +0000

    comments

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 
+"Look boxes"""
+
+
 def canUnlockAll(boxes):
+    """
+    ou have n number of locked boxes in front of you.
+    Each box is numbered sequentially from 0 to n - 1 and
+    each box may contain keys to the other boxes.
+    """
     thisset = set ()
-    
+
     for key in boxes:
         if (key):
             if key[0] < len(boxes):
