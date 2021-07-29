750acdb5641aeabfc1f6830bb28ea35385b1fbe3
root <evertescalante@gmail.com>
2021-07-29 00:04:27 +0000

    add script with recursion

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,41 +1,56 @@
 #include "lists.h"
-
+#include <stdio.h>
+listint_t *recursive_iter(listint_t **curr, listint_t  *new, int n);
 /**
- * insert_node - inset node in sorted linked list
- * @head: double pointer to head node
- * @number: number to save
+ * listint_t *insert_node - insert node in sorted list
+ * @head: double pointer to lis head
+ * @number: number to sort
  *
- * Return: new node or Null
+ * Return: new node
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new;
-	listint_t *current;
-	listint_t *currNext;
+	listint_t *new = NULL, *curr = NULL;
 
-	current = *head;
+	curr = *head;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	new->n = number;
-	new->next = NULL;
 
-	if (*head == NULL || number < (*head)->n)
+	new->next = NULL;
+	new->n = number;
+
+	if (curr == NULL || number < curr->n)
 	{
-		new->next = *head;
 		*head = new;
+		new->next = *head;
 		return (new);
 	}
-	currNext = current->next;
-	while (current->next != NULL)
+
+	return (recursive_iter(&curr->next, new, number));
+}
+
+/**
+ * listint_t *recursive_iter - recursive iteration
+ * @curr: current node place
+ * @new: node to set in the list
+ * @n: number to compare
+ *
+ * Return: Node on success
+ */
+listint_t *recursive_iter(listint_t **curr, listint_t  *new, int n)
+{
+	if (*curr && (*curr)->next)
 	{
-		if (number >= current->n && number <= currNext->n)
+		if (n >= (*curr)->n && n <= (*curr)->next->n)
 		{
-			current->next = new, new->next = currNext;
-			break;
+			new->next = (*curr)->next;
+			(*curr)->next = new;
+			return (new);
 		}
-		current = current->next, currNext = currNext->next;
+		else
+			recursive_iter(&(*curr)->next, new, n);
 	}
-	return (new);
+	return (NULL);
 }
---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number_while_loop.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * insert_node - inset node in sorted linked list
+ * @head: double pointer to head node
+ * @number: number to save
+ *
+ * Return: new node or Null
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *current;
+	listint_t *currNext;
+
+	current = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	new->next = NULL;
+
+	if (*head == NULL || number < (*head)->n)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	currNext = current->next;
+	while (current->next != NULL)
+	{
+		if (number >= current->n && number <= currNext->n)
+		{
+			current->next = new, new->next = currNext;
+			break;
+		}
+		current = current->next, currNext = currNext->next;
+	}
+	return (new);
+}
