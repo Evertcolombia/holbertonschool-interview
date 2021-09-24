deb5b033701bed75860e271eb642524d1fc7e1ca
evertcolombia <evertescalante@gmail.com>
2021-09-23 19:24:32 -0500

    executable files

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+
 from itertools import takewhile
 
 
 def to_bits(bytes):
+    """
+	convert in bits the bytes
+	:type bytes: List[int]
+    """
     for byte in bytes:
         num = []
         exp = 1 << 8
