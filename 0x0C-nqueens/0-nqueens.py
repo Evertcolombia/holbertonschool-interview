3e0aa95bcc143fa3da7cfec57afa652bfc97eba0
evertcolombia <evertescalante@gmail.com>
2021-11-20 18:43:14 -0500

    fixing method

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,95 +1,45 @@
 #!/usr/bin/python3
-""" This script solves the N-Queen problem"""
-
 
 import sys
 
 
-def check_moves(matrix, position_x, position_y):
+def nqueens(queens, xy_diff, xy_sum):
+    """Finds all valid queen column positions that don't attack each other
+    Args:
+        queens: columns occupied by queens
+        xy_diff: positive slope diagonals occupied by queens
+        xy_sum: negative slope diagonals occupied by queens
     """
-    This function checks if a position (x, y) has
-    has a 1 horizontal, vertical or diagonal
-    """
-    for x in range(len(matrix)):
-        for y in range(len(matrix[0])):
-            # Checks for horizontal queens
-            if (matrix[x][position_y] == 1):
-                return(0)
-            # Checks for vertical queens
-            if (matrix[position_x][y] == 1):
-                return(0)
-            # Diagonal Check for queens
-            try:
-                if (matrix[position_x + x][position_y + x] == 1):
-                    return(0)
-            except IndexError:
-                pass
-            try:
-                if (position_x - x >= 0):
-                    if (matrix[position_x - x][position_y + x] == 1):
-                        return(0)
-            except IndexError:
-                pass
-            try:
-                if (position_x - x >= 0 and position_y - x >= 0):
-                    if (matrix[position_x - x][position_y - x] == 1):
-                        return(0)
-            except IndexError:
-                pass
-            try:
-                if (position_y - x >= 0):
-                    if (matrix[position_x + x][position_y - x] == 1):
-                        return(0)
-            except IndexError:
-                pass
-    return(1)
+    p = len(queens)
+    if p == n:
+        queen_col.append(queens)
+        return None
+    for q in range(n):
+        if q not in queens and p - q not in xy_diff and p + q not in xy_sum:
+            nqueens(queens + [q], xy_diff + [p - q], xy_sum + [p + q])
 
 
-def put_coords(matrix, result):
-    """
-    This function print a pair of coords to result
-    """
-    for i in range(0, len(matrix)):
-        for j in range(0, len(matrix[0])):
-            if (matrix[i][j] == 1):
-                result[i][0] = i
-                result[i][1] = j
-    return result
-
-
-def recursive_chess(matrix, columns, n):
-    """
-    This function checks for every queen
-    if there's a queen atacking if not print
-    the n queens position
-    """
-    if (columns == n):
-        result = [[0 for x in range(2)] for y in range(n)]
-        print(put_coords(matrix, result))
-        return
-
-    for row in range(n):
-        if (check_moves(matrix, row, columns) == 1):
-            matrix[row][columns] = 1
-            recursive_chess(matrix, columns + 1, n)
-            matrix[row][columns] = 0
-
-
-if __name__ == '__main__':
-
+def parse_argv():
+    """Checks for usage errors"""
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
+    if not sys.argv[1].isdigit():
         print("N must be a number")
         sys.exit(1)
-
+    n = int(sys.argv[1])
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
+    return n
 
-    matrix = [[0 for j in range(n)] for i in range(n)]
-    recursive_chess(matrix, 0, n)
+
+if __name__ == "__main__":
+    n = parse_argv()
+    queen_col = []
+    nqueens([], [], [])
+    for row in range(len(queen_col)):
+        queen_pos = []
+        for col in range(len(queen_col[row])):
+            queen_pos.append([col, queen_col[row][col]])
+        print(queen_pos)
